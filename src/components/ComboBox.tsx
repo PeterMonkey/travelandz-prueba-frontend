d3931e01c2f936877a6081d64482eb7d8ecb96c8
Pedro Fernandez <pefernandez1408@gmail.com>
2024-03-01 16:35:27 -0400

    ADD: Creacion del componente ComboBox para cargar las opciones de filtrado

---
+++ src/components/ComboBox.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+  type Data = {
+    value: string,
+    label: string
+  }
+
+  type Props = {
+    data: Data[],
+    text: string
+  }
+
+export default function ComboBox({data, text}: Props) {
+    const [open, setOpen] = useState(false)
+    const [value, setValue] = useState("")
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-[200px] justify-between"
+            >
+              {value
+                ? data.find((element) => element.value === value)?.label
+                : text}
+              <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder={text} className="h-9" />
+              <CommandEmpty>No framework found.</CommandEmpty>
+              <CommandGroup>
+                {data.map((element) => (
+                  <CommandItem
+                    key={element.value}
+                    value={element.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false)
+                    }}
+                  >
+                    {element.label}
+                    <CheckIcon
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        value === element.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      )
+}
--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -1,12 +1,29 @@
-import { Card } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import CardTransfer from "@/components/CardTransfer"
+import ComboBox from '@/components/ComboBox'
+
+const frameworks = [
+    {
+      value: "ES",
+      label: "España",
+    },
+    {
+      value: "AR",
+      label: "Argentina",
+    },
+]
 
 export default function Panel() {
     return (
         <div className="flex justify-center items-center h-screen">
             <div className=" flex w-[90%] h-[90%]">
                 <Card className="w-1/4 h-full mr-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-
+                    <CardHeader>
+                        <CardTitle>Filtre sus servicios</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <ComboBox text="Pais..." data={frameworks}/>
+                    </CardContent>
                 </Card>
                 <div className="grid grid-cols-1 grid-rows-10 w-3/4">
                     <div className="grid grid-cols-3 grid-rows-2 gap-4 row-span-9 h-[90%]">
